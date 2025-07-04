b0f91ef4b449f712579864aaadd0f087b50d68b6
bhonepyisone <bhone.ps@proton.me>
2025-07-04 20:03:21 +0700

    Use environment variables for Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyBrGs8lyhw-qawWQZ1Qcj6yUffN79Gdh0U",
-  authDomain: "vozz-os.firebaseapp.com",
-  projectId: "vozz-os",
-  storageBucket: "vozz-os.firebasestorage.app",
-  messagingSenderId: "128985569447",
-  appId: "1:128985569447:web:1b5958a3330a2bbbfd6382"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
